e57883d95114dc01d652dbb39da1fa12128c6a06
David Sheridan <dsheridan2@gmail.com>
2024-09-19 16:47:28 -0400

    Commit 1

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author dsher
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is a test of my git capabilities.");
+    }
+}
